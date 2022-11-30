2efbb91c93f6028cdaf3856ae0839f70a3ea110e
Robert Takizawa <robtak223@gmail.com>
2022-11-30 00:13:05 -0500

    commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const fetchVideos = async () => {
   const options = {
    method: 'GET',
-   url: `https://youtube.googleapis.com/youtube/v3/search? part=snippet&maxResults=50&id=dQw4w9WgXcQ&key=AIzaSyDrCOtNiJ4KjHhkOI15YaOd8MacXyNoWZM`
+   url: `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&id=dQw4w9WgXcQ&key=AIzaSyDrCOtNiJ4KjHhkOI15YaOd8MacXyNoWZM`
    };
   const result = await axios(options)
   return result.data
