f1fab1fa751371ff2744749c5d0f627621a565c7
Robert Takizawa <robtak223@gmail.com>
2022-12-05 22:23:42 -0500

    complete refresh

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-const fetchVideos = async () => {
-  const options = {
-   method: 'GET',
-   url: `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&id=dQw4w9WgXcQ&key=AIzaSyDrCOtNiJ4KjHhkOI15YaOd8MacXyNoWZM`
-   };
-  const result = await axios(options)
-  return result.data
-}
-
 router.get('/contact', function(req, res, next) {
   res.render('contact/contact', { title: "Contact"});
 });
