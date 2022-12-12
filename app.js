bf74fa4a9bcb6d676667c3098509da7845402b02
Robert Takizawa <robtak223@gmail.com>
2022-12-12 14:44:12 -0500

    prelim changes

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
-// sendFile will go here
+
+app.get('/contact', function(req, res) {
+    res.sendFile(path.join(__dirname, '/views/contact.html'));
+  });
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
